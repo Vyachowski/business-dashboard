c63399d6eb95a7d544edfd49928909a1f9edd62b
Slava Haikin <97975418+Vyachowski@users.noreply.github.com>
2023-10-10 20:58:01 +0300

    feat: Add header for key metrics section

--- client/src/pages/Dashboard/Overview.tsx
+++ client/src/pages/Dashboard/Overview.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ChartSeven from '../../components/Charts/ChartSeven';
 import DataStatsOne from "../../components/DataStats/DataStatsOne.tsx";
 import DataStats from "../../components/DataStats/DataStats.tsx";
+import ChartFour from "../../components/Charts/ChartFour.tsx";
+
 
 const Overview: React.FC = () => {
   return (
@@ -25,8 +27,35 @@ const Overview: React.FC = () => {
       </div>
 
       <div className="mt-7.5 grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
+        <div className="col-span-6">
+          <h2 className="text-title-sm2 font-bold text-black dark:text-white">
+            Key Metrics
+          </h2>
+        </div>
         <div className="col-span-12">
           <DataStats />
+        </div>
+      </div>
+
+      <div className="mt-7.5 grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
+        <div className="col-span-6">
+          <h2 className="text-title-sm2 font-bold text-black dark:text-white">
+            Revenue trend
+          </h2>
+        </div>
+        <div className="col-span-12">
+          <ChartFour />
+        </div>
+      </div>
+
+      <div className="mt-7.5 grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
+        <div className="col-span-6">
+          <h2 className="text-title-sm2 font-bold text-black dark:text-white">
+            Leads trend
+          </h2>
+        </div>
+        <div className="col-span-12">
+          <ChartFour />
         </div>
       </div>
     </>
